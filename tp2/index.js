5574a50b0f88e9e10a4107b545f9811468b42930
chaimaalaya <chaima.alaya@polytechnicien.tn>
2025-02-13 02:12:13 +0100

    tp2

---
+++ tp2/index.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const db = require('./database');
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+const memoryStore = new session.MemoryStore();
+app.use(session({
+secret: 'api-secret',
+resave: false,
+saveUninitialized: true,
+store: memoryStore
+}));
+// Configuration de Keycloak
+const keycloak = new Keycloak({ store: memoryStore }, './keycloak-config.json');
+app.use(keycloak.middleware());
+// Exemple : Protéger une route avec Keycloak
+app.get('/secure', keycloak.protect(), (req, res) => {
+    res.json({ message: 'Vous êtes authentifié !' });
+    });
+// Exemple de sécurisation d'une route existante
+app.get('/personnes', keycloak.protect(), (req, res) => {
+    db.all("SELECT * FROM personnes", [], (err, rows) => {
+    if (err) {
+    res.status(400).json({ "error": err.message });
+    return;
+    }
+    res.json({ "message": "success", "data": rows });
+    });
+    });
+/* app.get('/', (req, res) => {
+res.json("Registre de personnes! Choisissez le bon routage!")
+}) */
+
+// Récupérer toutes les personnes
+/* app.get('/personnes', (req, res) => {
+db.all("SELECT * FROM personnes", [], (err, rows) => {
+if (err) {
+    res.status(400).json({
+    "error": err.message
+    });
+return;
+}
+res.json({
+    "message": "success",
+    "data": rows
+    });
+});
+}); */
+// Récupérer une personnesne par ID
+app.get('/personnes/:id', (req, res) => {
+    const id = req.params.id;
+    db.get("SELECT * FROM personnes WHERE id = ?", [id], (err, row) => {
+    if (err) {
+        res.status(400).json({
+        "error": err.message
+        });
+    return;
+    }
+    res.json({
+    "message": "success",
+    "data": row });
+    });
+});
+// Créer une nouvelle personnesne
+    app.post('/personnes', (req, res) => {
+    const nom = req.body.nom;
+    const adresse = req.body.adresse;
+    db.run(`INSERT INTO personnes (nom,adresse) VALUES (?,?)`, [nom,adresse], function(err) {
+    if (err) {
+    res.status(400).json({
+    "error": err.message
+    });
+    return; }
+    res.json({
+        "message": "success",
+        "data": {
+        id: this.lastID }
+        });
+    });
+    });
+// Mettre à jour une personnesne
+app.put('/personnes/:id', (req, res) => {
+    const id = req.params.id;
+    const nom = req.body.nom;
+    const adresse = req.body.adresse;
+    
+
+    db.run(`UPDATE personnes SET nom = ? , adresse = ?  WHERE id = ?`, [nom, adresse, id], function(err) {
+    if (err) {
+    res.status(400).json({
+    "error": err.message
+    });
+    return; }
+        res.json({
+        "message": "success"
+        });
+    });
+    });
+// Supprimer une personnesne
+    app.delete('/personnes/:id', (req, res) => {
+    const id = req.params.id;
+    db.run(`DELETE FROM personnes WHERE id = ?`, id, function(err) {
+    if (err) {
+    res.status(400).json({
+    "error": err.message
+    });
+    return;
+    }
+    res.json({
+    "message": "success" });
+    });
+    });
+    
+    app.listen(PORT, () => {console.log(`Server running on port ${PORT}`); });
